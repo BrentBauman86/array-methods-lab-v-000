e5db8cd554ea0324980bf18832565c986173e112
Brent Bauman <brentbauman86@gmail.com>
2018-04-12 22:40:03 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@
 
 def using_include(array, element)
 #binding.pry
-array.include?("wow") == array.include?("element2")
+array.include?("wow") != array.include?("element2")
 end
 
 def using_sort(array)
